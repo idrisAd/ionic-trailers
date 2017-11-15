b599c989b41ac2a1a7b3183c1fb19fb34eb40b0e
Unknown <idrisadrien@hotmail.fr>
2017-11-15 15:06:33 +0100

    structure login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,8 +8,17 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
 
+  }
+
+  showAlert()  {
+    let alert = this.alertCtrl.create({
+      title: 'Sign in',
+      subTitle: 'Test button',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
   ionViewDidLoad() {
