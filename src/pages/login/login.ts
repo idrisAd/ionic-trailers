bc7614ad53360f0248dc79c7f2cac00db439575b
Idris Adrien <32459686+idrisAd@users.noreply.github.com>
2017-11-15 22:36:00 +0100

    update login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,32 +1,49 @@
 import { Component } from '@angular/core';
-import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Events, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Storage} from "@ionic/storage"
+
 
 @IonicPage()
 @Component({
-
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  private user: FormGroup;
+  remember: boolean = false;
 
-  }
-
-  showAlert()  {
-    let alert = this.alertCtrl.create({
-      title: 'Sign in',
-      subTitle: 'Test button',
-      buttons: ['OK']
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private events: Events, private storage: Storage) {
+    this.user = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
     });
-    alert.present();
-  }
-
-  logForm(form){
-
+    storage.get('email').then((val) => {
+      this.user.setValue({email: val, password: ''});
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+  dismiss() {
+    this.navCtrl.pop().then();
+  }
+  logForm() {
+    console.log(this.user.value);
+    if(this.user.value.password === "azerty") {
+      this.events.publish("user.login");
+      this.navCtrl.pop().then(() => console.log('Log succes'));
+      if(this.remember) {
+        this.storage.set('email', this.user.value.email).then(() => console.log('email added'));
+      }
+    } else {
+      alert('Wrong password');
+    }
+  }
+  checkboxToggle() {
+    this.remember = !this.remember;
+    console.log(this.remember);
   }
 
 }
